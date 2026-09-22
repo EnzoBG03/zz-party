2b5dad41eec0e7db7e3f85ee68a5c4ca96b26918
Enzo_BG <153947784+EnzoBG03@users.noreply.github.com>
2026-09-22 16:08:51 +0200

    Création du gestionnaire d'entrées

---
+++ Scripts/Core/InputController.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace ZZParty;
+
+public partial class InputController : Node
+{
+    public bool EstToucheActionPressee(int idJoueur)
+    {
+        return false;
+    }
+
+    public bool EstToucheAnnulationPressee(int idJoueur)
+    {
+        return false;
+    }
+
+    public Vector2 ObtenirVecteurMouvement(int idJoueur)
+    {
+        return Vector2.Zero;
+    }
+}
